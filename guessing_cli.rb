115db0165074c067040428b483a04901e79702cc
Scoti Dodson <scotidodson@gmail.com>
2018-09-12 18:59:31 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -6,7 +6,7 @@ def run_guessing_game
     number = rand(1..6).to_s
   case guess.chomp
     when number
-      return  "You guessed the correct number!"
+      puts  "You guessed the correct number!"
     when "exit" 
       puts "Goodbye!"
       break
