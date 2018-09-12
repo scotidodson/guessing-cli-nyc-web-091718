9918a7284a2bd1f5cdd90c52640721e733f804c6
Scoti Dodson <scotidodson@gmail.com>
2018-09-12 19:00:13 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,4 +1,6 @@
 def run_guessing_game
+  input = ""
+  while input
     puts "Guess a number between 1 and 6."
     guess = gets.downcase.chomp
     number = rand(1..6).to_s
@@ -7,10 +9,11 @@ def run_guessing_game
       puts  "You guessed the correct number!"
     when "exit" 
       puts "Goodbye!"
-      return
+      break
     else
       puts "The computer guessed #{number}!"
     end
+  end 
 end
 
 
